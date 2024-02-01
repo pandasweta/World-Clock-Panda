d687105d1cf61076adb11a54c8478b25979ad2fc
pandasweta <130973588+pandasweta@users.noreply.github.com>
2024-02-01 23:38:36 +0100

    updated the time and date of the cities from the list.
    Added the basic JS for the cities from the list. Worked around the date and time, so that it reflects when we select the city from the list.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,53 @@
 function displayTime() {
     // City-1 date and time
     let newDelhiElement = document.querySelector("#new-delhi");
-    let newDelhiTime = document.querySelector("#city-time");
-    newDelhiTime.innerHTML = `${moment().tz("Asia/Kolkata").format("hh:mm:ss [<small>]A[<small>]")}`;
-    setInterval(time, 1000); let newDelhiDate = document.querySelector("#city-date");
-    newDelhiDate.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+    if (newDelhiElement) {
+        let newDelhiTime = document.querySelector("#city-time");
+        // let time = moment().tz("Asia/Kolkata").format("hh:mm:ss [<small>]A[<small>]");
+        newDelhiTime.innerHTML = `${moment().tz("Asia/Kolkata").format("hh:mm:ss [<small>]A[<small>]")}`;
+        let newDelhiDate = document.querySelector("#city-date");
+        newDelhiDate.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+    }
 
     // City-2 date and time
 
-    let rotterdamElement = document.querySelector("#rotterdam");
-    let rotterdamTime = document.querySelector("#time");
-    rotterdamTime.innerHTML = `${moment.tz("Europe/Amsterdam").format("hh:mm:ss [<small>]A[<small>]")}`;
-    let rotterdamDate = document.querySelector("#date");
-    rotterdamDate.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+    let rotterDamElement = document.querySelector("#rotterdam");
+    if (rotterDamElement) {
+        let rotterdamTime = document.querySelector("#time");
+        rotterdamTime.innerHTML = `${moment.tz("Europe/Amsterdam").format("hh:mm:ss [<small>]A[<small>]")}`;
+        let rotterdamDate = document.querySelector("#date");
+        rotterdamDate.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+    }
 }
 
 displayTime();
 setInterval(displayTime, 1000);
+
+
+// update date and time for the cities from list
+
+function showCitiesTimeDate(event) {
+
+    let TimeZone = event.target.value;
+    let cityTimeZone = moment().tz(TimeZone);
+    let cityName = TimeZone.replace("_", " ").split("/")[1];
+    // let cityTime = moment().tz(TimeZone).format("hh:mm:ss [<small>]A[<small>]");
+    let cityDate = moment().tz(TimeZone).format("MMMM Do YYYY");
+    let cityNameElement = document.querySelector("#cities");
+    cityNameElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityDate}</div>
+          </div>
+          <div class="time">${cityTimeZone.format("hh:mm:ss A")}</div>
+    </div>`
+
+
+}
+
+
+
+
+let cityName = document.querySelector("#select-cities");
+cityName.addEventListener("change", showCitiesTimeDate);
 
